03e1fb97fc7ef5fc73b1ef81df48b7c0fa021720
BoyuZhou <386607913@qq.com>
2018-01-04 21:01:21 +0800

    add: new props list

--- src/ButtonGroup.js
+++ src/ButtonGroup.js
@@ -1,52 +1,90 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, {Component} from 'react'
 import classNames from 'classnames';
-// import Button from 'bee-button';
+import Button from 'bee-button';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  /**
-   * 是否垂直排列
-  */
-  vertical: PropTypes.bool,
-  /**
-   * 是否对齐
-   */
-  justified: PropTypes.bool,
+    /**
+     * 是否垂直排列
+     */
+    vertical: PropTypes.bool,
+    /**
+     * 是否对齐
+     */
+    justified: PropTypes.bool,
 
-  /**
-   * 垂直时是否为块状元素
-   */
-  block: PropTypes.bool
+    /**
+     * 垂直时是否为块状元素
+     */
+    block: PropTypes.bool,
+    list: PropTypes.array
 };
 
 const defaultProps = {
-  block: false,
-  justified: false,
-  vertical: false
+    block: false,
+    justified: false,
+    vertical: false,
+    list: []
 };
 
 const clsPrefix = "u-button-group";
 
 class ButtonGroup extends React.Component {
-  render() {
-    const { block, justified, vertical, classes, ...others } = this.props;
-    const tbClass = {
-        [`${clsPrefix}`]: vertical ? false : true,
-        [`${clsPrefix}-block`]: vertical ? block : false,
-        [`${clsPrefix}-vertical`]: vertical,
-        [`${clsPrefix}-justified`]: justified
-    };
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            activeKey: ''
+        }
+    }
 
-    return (
-      <div
-        {...others}
-        className={classNames(tbClass, classes)}
-      >
-      { this.props.children }
-      </div>
-    );
-  }
+    handleItemClick = (key, onClick) => (e) => {
+        this.setState({
+            activeKey: key
+        })
+        onClick && onClick(e);
+    }
+    render() {
+        const {block, justified, vertical, className, list, ...others} = this.props;
+        const tbClass = {
+            [`${clsPrefix}`]: vertical ? false : true,
+            [`${clsPrefix}-block`]: vertical ? block : false,
+            [`${clsPrefix}-vertical`]: vertical,
+            [`${clsPrefix}-justified`]: justified
+        };
+        if(list.length === 0){
+            return (
+                <div
+                    {...others}
+                    className={classNames(tbClass, className)}
+                >
+                    { this.props.children }
+                </div>
+            );
+        }
+        console.log(list);
+
+        return (
+            <div
+                {...others}
+                className={classNames(tbClass, className)}
+            >
+                {
+                    list.map((item) => {
+                        let { title, onClick, className,...otherProps } = item;
+                        return (
+                            <Button
+                                className={ classNames(className, {'active': this.state.activeKey === item.key})}
+                                onClick={ this.handleItemClick(item.key, onClick) }
+                                {...otherProps}>
+                                { title }
+                            </Button>
+                        )
+                    })
+                }
+            </div>
+        )
+
+    }
 }
 
 ButtonGroup.propTypes = propTypes;

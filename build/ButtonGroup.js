4342db589e70c5f088b1e6819db52f4a04a638d3
BoyuZhou <386607913@qq.com>
2018-01-05 09:25:51 +0800

    feat(bee-button-group): 新增list接口，可传入数据和props，来渲染button，并且提供选中样式

--- build/ButtonGroup.js
+++ build/ButtonGroup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
@@ -10,13 +10,13 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = require('react-dom');
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
 var _classnames = require('classnames');
 
 var _classnames2 = _interopRequireDefault(_classnames);
+
+var _beeButton = require('bee-button');
+
+var _beeButton2 = _interopRequireDefault(_beeButton);
 
 var _propTypes = require('prop-types');
 
@@ -35,64 +35,106 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : _defaults(subClass, superClass); }
-// import Button from 'bee-button';
-
 
 var propTypes = {
-  /**
-   * 是否垂直排列
-  */
-  vertical: _propTypes2["default"].bool,
-  /**
-   * 是否对齐
-   */
-  justified: _propTypes2["default"].bool,
+    /**
+     * 是否垂直排列
+     */
+    vertical: _propTypes2["default"].bool,
+    /**
+     * 是否对齐
+     */
+    justified: _propTypes2["default"].bool,
 
-  /**
-   * 垂直时是否为块状元素
-   */
-  block: _propTypes2["default"].bool
+    /**
+     * 垂直时是否为块状元素
+     */
+    block: _propTypes2["default"].bool,
+    /**
+     * 传入数组替代button
+     */
+    list: _propTypes2["default"].array
 };
 
 var defaultProps = {
-  block: false,
-  justified: false,
-  vertical: false
+    block: false,
+    justified: false,
+    vertical: false,
+    list: []
 };
 
 var clsPrefix = "u-button-group";
 
 var ButtonGroup = function (_React$Component) {
-  _inherits(ButtonGroup, _React$Component);
+    _inherits(ButtonGroup, _React$Component);
 
-  function ButtonGroup() {
-    _classCallCheck(this, ButtonGroup);
+    function ButtonGroup(props, context) {
+        _classCallCheck(this, ButtonGroup);
 
-    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
-  }
+        var _this = _possibleConstructorReturn(this, _React$Component.call(this, props, context));
 
-  ButtonGroup.prototype.render = function render() {
-    var _tbClass;
+        _this.handleItemClick = function (key, onClick) {
+            return function (e) {
+                _this.setState({
+                    activeKey: key
+                });
+                onClick && onClick(e);
+            };
+        };
 
-    var _props = this.props,
-        block = _props.block,
-        justified = _props.justified,
-        vertical = _props.vertical,
-        classes = _props.classes,
-        others = _objectWithoutProperties(_props, ['block', 'justified', 'vertical', 'classes']);
+        _this.state = {
+            activeKey: ''
+        };
+        return _this;
+    }
 
-    var tbClass = (_tbClass = {}, _defineProperty(_tbClass, '' + clsPrefix, vertical ? false : true), _defineProperty(_tbClass, clsPrefix + '-block', vertical ? block : false), _defineProperty(_tbClass, clsPrefix + '-vertical', vertical), _defineProperty(_tbClass, clsPrefix + '-justified', justified), _tbClass);
+    ButtonGroup.prototype.render = function render() {
+        var _tbClass,
+            _this2 = this;
 
-    return _react2["default"].createElement(
-      'div',
-      _extends({}, others, {
-        className: (0, _classnames2["default"])(tbClass, classes)
-      }),
-      this.props.children
-    );
-  };
+        var _props = this.props,
+            block = _props.block,
+            justified = _props.justified,
+            vertical = _props.vertical,
+            className = _props.className,
+            list = _props.list,
+            others = _objectWithoutProperties(_props, ['block', 'justified', 'vertical', 'className', 'list']);
 
-  return ButtonGroup;
+        var tbClass = (_tbClass = {}, _defineProperty(_tbClass, '' + clsPrefix, vertical ? false : true), _defineProperty(_tbClass, clsPrefix + '-block', vertical ? block : false), _defineProperty(_tbClass, clsPrefix + '-vertical', vertical), _defineProperty(_tbClass, clsPrefix + '-justified', justified), _tbClass);
+        if (list.length === 0) {
+            return _react2["default"].createElement(
+                'div',
+                _extends({}, others, {
+                    className: (0, _classnames2["default"])(tbClass, className)
+                }),
+                this.props.children
+            );
+        }
+
+        return _react2["default"].createElement(
+            'div',
+            _extends({}, others, {
+                className: (0, _classnames2["default"])(tbClass, className)
+            }),
+            list.map(function (item) {
+                var title = item.title,
+                    onClick = item.onClick,
+                    className = item.className,
+                    otherProps = _objectWithoutProperties(item, ['title', 'onClick', 'className']);
+
+                return _react2["default"].createElement(
+                    _beeButton2["default"],
+                    _extends({
+                        className: (0, _classnames2["default"])(className, { 'active': _this2.state.activeKey === item.key }),
+                        onClick: _this2.handleItemClick(item.key, onClick)
+                    }, otherProps),
+                    title
+                );
+            })
+        );
+    };
+
+    return ButtonGroup;
 }(_react2["default"].Component);
 
 ButtonGroup.propTypes = propTypes;
